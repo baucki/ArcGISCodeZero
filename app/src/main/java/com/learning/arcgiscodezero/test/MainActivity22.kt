83067bb44ba79113352e2bbadeb141042b0a47d2
baucki <ibauk6218rn@raf.rs>
2024-05-07 15:21:57 +0200

    displayed my map with pins for restaurants in a place

---
+++ app/src/main/java/com/learning/arcgiscodezero/test/MainActivity22.kt
@@ -0,0 +1,90 @@
+package com.learning.arcgiscodezero.test
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.arcgismaps.ApiKey
+import com.arcgismaps.ArcGISEnvironment
+import com.arcgismaps.mapping.ArcGISMap
+import com.arcgismaps.mapping.BasemapStyle
+import com.arcgismaps.mapping.PortalItem
+import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.mapping.view.GraphicsOverlay
+import com.arcgismaps.portal.Portal
+import com.arcgismaps.tasks.geocode.LocatorTask
+import com.arcgismaps.toolkit.geoviewcompose.MapView
+import com.learning.arcgiscodezero.R
+import com.learning.arcgiscodezero.ui.theme.ArcGISCodeZeroTheme
+
+class MainActivity22: ComponentActivity() {
+    // "https://utility.arcgis.com/usrsvcs/servers/a2c12b9d24e14b1f9ee0829912dc3855/rest/services/World/GeocodeServer"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setApiKey()
+
+        setContent {
+            ArcGISCodeZeroTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+
+    private fun createMap(): ArcGISMap {
+        val portal = Portal(
+            url = "https://www.arcgis.com",
+            connection = Portal.Connection.Anonymous
+        )
+
+        val portalItem = PortalItem(
+            portal = portal,
+            itemId = "90212b5a14fb423682d5d705573b6650"
+        )
+
+        return ArcGISMap(portalItem)
+    }
+
+    private fun setApiKey() {
+        ArcGISEnvironment.apiKey = ApiKey.create("AAPK1e43bdcf9fa04fa0a729106fdd7a97fbNbpa3VVhaR5eKzfmkAFb0Uy_soNrGAjpslTJLcWQiNV6T3YGoRy8Sfa7a5ZXkBcj")
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    private fun MainScreen() {
+
+        val map = remember {
+            createMap()
+        }
+
+        Scaffold(
+            topBar = { TopAppBar(title = { Text(text = stringResource(id = R.string.app_name)) }) }
+        ) {
+            MapView(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it),
+                arcGISMap = map
+            )
+        }
+
+    }
+
+}
